78bdba69d0153497fa132e301f3354ba4ed2e0b9
herve1105 <mazumbubiyela@users.noreply.github.com>
2025-10-10 17:29:16 +0100

    feat:variable declaration and assignment of a numeric value, string, boolean, null value. Then unknown variable without giving it a value.

--- 02-type-donnes/exercice_6/main.js
+++ 02-type-donnes/exercice_6/main.js
@@ -1,2 +1,8 @@
 // Exercice 6
 // Écrivez votre code ici
+let age = 25;
+let nom = "Kadea";
+let estConnecte = true;
+let valeurNulle = null;
+let inconnue;
+console.log(age, nom, estConnecte, valeurNulle, inconnue);
