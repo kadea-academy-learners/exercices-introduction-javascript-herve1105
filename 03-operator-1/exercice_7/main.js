dc3b78d1b19c4ce5349cfac001f996b278050787
herve1105 <mazumbubiyela@users.noreply.github.com>
2025-10-12 00:45:25 +0100

    feat: add: algorithm for family age calculations

--- 03-operator-1/exercice_7/main.js
+++ 03-operator-1/exercice_7/main.js
@@ -1,2 +1,13 @@
 // Exercice 7
 // Écrivez votre code ici
+let ageEnfant = 10;
+let agePere = ageEnfant * 2;
+let ageMere = agePere - 5;
+let ageGrandPere = (ageMere * 2) + (ageEnfant / 2);
+let ageOncle = agePere + 10;
+
+console.log("Age de l'enfant :", ageEnfant);
+console.log("Age du père :", agePere);
+console.log("Age de la mère :", ageMere);
+console.log("Age du grand-père :", ageGrandPere);
+console.log("Age de l'oncle :", ageOncle);
